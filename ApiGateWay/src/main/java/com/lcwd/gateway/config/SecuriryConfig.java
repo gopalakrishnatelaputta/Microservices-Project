de4a2ed1c585122cc829b9c29c1244ac5bf51224
gopalakrishnatelaputta <121857875+gopalakrishnatelaputta@users.noreply.github.com>
2023-05-24 10:06:43 +0530

    Add files via upload

---
+++ ApiGateWay/src/main/java/com/lcwd/gateway/config/SecuriryConfig.java
@@ -0,0 +1,29 @@
+package com.lcwd.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecuriryConfig {
+    @Bean
+    public SecurityWebFilterChain securiryWenFilterChain(ServerHttpSecurity httpSecurity)
+    {
+        httpSecurity
+                .authorizeExchange()
+                .anyExchange()
+                .authenticated()
+                .and()
+                .oauth2Login()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        return httpSecurity.build();
+
+    }
+}
